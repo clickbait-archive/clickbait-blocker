83889aac602fe752a0b2cbd3e46ff2e4b36f21e5
Ian Carrico <git@iancarrico.com>
2016-08-18 11:50:05 -0500

    Initial commits of functionality and actual blocking

--- scripts/content_scripts.js
+++ scripts/content_scripts.js
@@ -9,7 +9,8 @@
   var ClickBaitBlocker = {
     whitelist: null,
     blacklist: null,
-    configuration: null
+    configuration: null,
+    baseId: 'clickbaitblocker__'
   };
 
   /**
@@ -27,13 +28,15 @@
 
     // Our three checks. Is it on either of the blacklists? Is it on the
     // whitelist?
-    var onBlacklist = ClickBaitBlocker.blacklist.includes(ClickBaitBlocker.currentHostname);
-    var onWhitelist = ClickBaitBlocker.whitelist.includes(ClickBaitBlocker.currentHostname);
-    var onBuiltinBlacklist = ClickBaitBlocker.configuration.blocked_sites.includes(ClickBaitBlocker.currentHostname);
+    ClickBaitBlocker.checks = {
+      onBlacklist: ClickBaitBlocker.blacklist.includes(ClickBaitBlocker.currentHostname),
+      onWhitelist: ClickBaitBlocker.whitelist.includes(ClickBaitBlocker.currentHostname),
+      onBuiltinBlacklist: ClickBaitBlocker.configuration.blocked_sites.includes(ClickBaitBlocker.currentHostname)
+    };
 
     // If this site is on either black list, and not on the allowed whitelist—
     // let us get rid of it.
-    if ((onBlacklist || onBuiltinBlacklist) && !onWhitelist) {
+    if ((ClickBaitBlocker.checks.onBlacklist || ClickBaitBlocker.checks.onBuiltinBlacklist) && !ClickBaitBlocker.checks.onWhitelist) {
       // Well, we don't want our eyeballs to care about this site. Let's break
       // it. Break it into small pieces— well... just hide it. But breaking
       // sounds more cool.
@@ -42,10 +45,64 @@
   }
 
   /**
+   * Ignore the block. Just remove the cover div.
+   */
+  var ignoreTheBlock = function() {
+    var cover = document.findElementById(ClickBaitBlocker.baseId + 'cover');
+  }
+
+  /**
    * Will take a modal, and cover our site with it.
    */
   var breakTheSite = function() {
+    var buttonClass = ClickBaitBlocker.baseId + 'buttons';
 
+
+    var coverDiv = document.createElement('div');
+        coverDiv.id = ClickBaitBlocker.baseId + 'cover';
+
+    var innerDiv = document.createElement('div');
+        innerDiv.id = ClickBaitBlocker.baseId + 'inner';
+
+    var titleHeader = document.createElement('h2');
+        titleHeader.id = ClickBaitBlocker.baseId + 'title';
+        titleHeader.innerHTML = 'This site is on the Click Bait Blocker list.'
+
+    // Reason if we have this site blocked.
+    var reason = 'This site is on our community-based block list. It has been determined that this site is mostly clickbait. If you would like to unblock this site, and be able to access it— you may add it to your personal whitelist or ignore this warning.';
+    var buttonAction = 'Add to whitelist';
+    var buttonValue = 'whitelist';
+
+    // If this was on our personal blacklist, the reason is very different.
+    if (ClickBaitBlocker.checks.onBlacklist) {
+      reason = 'This site has been added to your personal block list. You can remove it from your personal block list, or ignore this warning.';
+      buttonAction = 'Remove from blacklist';
+      buttonValue = 'blacklist';
+    }
+
+    var whyBlocked = document.createElement('p');
+        whyBlocked.id = ClickBaitBlocker.baseId + 'why-blocked';
+        whyBlocked.innerHTML = reason;
+
+    var ignoreBlock = document.createElement('button');
+        ignoreBlock.id = ClickBaitBlocker.baseId + 'ignore';
+        ignoreBlock.innerHTML = 'Ignore';
+        ignoreBlock.value = 'ignore';
+        ignoreBlock.class = buttonClass;
+
+    var actionBlock = document.createElement('button');
+        actionBlock.id = ClickBaitBlocker.baseId + 'action';
+        actionBlock.innerHTML = buttonAction;
+        actionBlock.value = buttonValue;
+        actionBlock.class = buttonClass;
+
+
+    innerDiv.appendChild(titleHeader);
+    innerDiv.appendChild(whyBlocked);
+    innerDiv.appendChild(ignoreBlock);
+    innerDiv.appendChild(actionBlock);
+    coverDiv.appendChild(innerDiv);
+    document.getElementsByTagName('body')[0].appendChild(coverDiv);
   }
 
   // What is the hostname we are currently on?
@@ -63,7 +120,7 @@
     }
   };
   // Make the XHR request, getting the local URL via Chrome's extension code.
-  xhr.open("GET", chrome.extension.getURL('/configuration/configuration.json'), true);
+  xhr.open('GET', chrome.extension.getURL('/configuration/configuration.json'), true);
   xhr.send();
 
   // Grab a user's personal settings on what to block and what to always show.
